34f85a601ead0522e8e55cc4a454fa746c83ba1a
Asila Maksumova <asilam@cs.washington.edu>
2020-02-08 16:12:21 -0800

    Added redirect onclick for states

--- src/uStates.js
+++ src/uStates.js
@@ -70,7 +70,10 @@
 		d3.select(id).selectAll(".state")
 			.data(uStatePaths).enter().append("path").attr("class","state").attr("d",function(d){ return d.d;})
 			.style("fill",function(d){ return data[d.id].color; })
-			.on("mouseover", mouseOver).on("mouseout", mouseOut);
+			.on("mouseover", mouseOver).on("mouseout", mouseOut)
+			.on("click", function() {
+				window.location.href = "http://www.google.com";
+			});
 	}
 	this.uStates=uStates;
 })();
